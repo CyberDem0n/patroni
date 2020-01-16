b53797c087e210a6ecc409f14d29269417a40c32
Alexander Kukushkin <alexander.kukushkin@zalando.de>
2020-01-16 15:50:49 +0100

    windows compat

--- tests/test_postmaster.py
+++ tests/test_postmaster.py
@@ -66,6 +66,7 @@ class TestPostmasterProcess(unittest.TestCase):
     @patch('psutil.Process.__init__', Mock())
     @patch('psutil.Process.send_signal')
     @patch('psutil.Process.pid', Mock(return_value=123))
+    @patch('os.name', 'posix')
     @patch('signal.SIGQUIT', 3, create=True)
     def test_signal_stop(self, mock_send_signal):
         proc = PostmasterProcess(-123)
