601e111c8221a97e6bf2314cb673d90a57020951
Alexander Kukushkin <alexander.kukushkin@zalando.de>
2020-01-20 20:42:42 +0100

    debug win

--- .github/workflows/run_tests.py
+++ .github/workflows/run_tests.py
@@ -1,5 +1,4 @@
 import os
-import string
 import subprocess
 import sys
 
@@ -24,9 +23,7 @@ def main():
     env['DCS'] = what
     command = unbuffer + [sys.executable, '-m', 'behave']
     if os.name == 'nt':
-        symbols = list(string.printable.strip())
-        subprocess.call(['net', 'user', 'postgres', '', '/ADD'])
-        command = ['runas', '/user:postgres', ' '.join(command)]
+        command = ['cmd', '/min', '/C', 'set __COMPAT_LAYER=RUNASINVOKER && start ' + ' '.join(command)]
     if subprocess.call(command, env=env) != 0:
         if subprocess.call('grep . features/output/*_failed/*postgres?.*', shell=True) != 0:
             subprocess.call('grep . features/output/*/*postgres?.*', shell=True)
