22f8b2d783ae7a50525fcd4c39f8ec6b72e11e40
Alexander Kukushkin <alexander.kukushkin@zalando.de>
2020-07-21 15:37:11 +0200

    Debug win

--- .github/workflows/run_tests.py
+++ .github/workflows/run_tests.py
@@ -22,9 +22,9 @@ def main():
     env['PATH'] = path + os.pathsep + env['PATH']
     env['DCS'] = what
     if os.name == 'nt':
-        subprocess.call(['initdb', '-D', 'data', '-U', 'postgres'])
-        subprocess.call(['pg_ctl', '-D', 'data', 'start'])
-        subprocess.call(['psql', '-U', 'postgres', '-c', 'SELECT version()'])
+        subprocess.call(['initdb', '-D', 'data', '-U', 'postgres'], env=env)
+        subprocess.call(['pg_ctl', '-D', 'data', 'start'], env=env)
+        subprocess.call(['psql', '-U', 'postgres', '-c', 'SELECT version()'], env=env)
         return 0
     if subprocess.call(unbuffer + [sys.executable, '-m', 'behave'], env=env) != 0:
         if subprocess.call('grep . features/output/*_failed/*postgres?.*', shell=True) != 0:
