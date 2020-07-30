41107dcacfc02f314a4d440d2fdb77b3057be65b
Alexander Kukushkin <alexander.kukushkin@zalando.de>
2020-07-30 20:32:10 +0200

    Debug windows

--- patroni/postgresql/rewind.py
+++ patroni/postgresql/rewind.py
@@ -284,8 +284,8 @@ class Rewind(object):
                 b += len(pattern)
                 e = line.find('": ', b)
                 if e > -1:
-                    waldir, wal_filename = os.path.split(line[b:e])
-                    if waldir.endswith(os.path.sep + 'pg_' + self._postgresql.wal_name) and len(wal_filename) == 24:
+                    waldir, wal_filename = line[b:e].rsplit('/', 1)
+                    if waldir.endswith('/pg_' + self._postgresql.wal_name) and len(wal_filename) == 24:
                         return wal_filename
 
     def pg_rewind(self, r):
