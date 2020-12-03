649b0d59e38696736bb76382f2cf8b10a65a4ce1
Alexander Kukushkin <alexander.kukushkin@zalando.de>
2020-12-03 19:52:32 +0100

    debug win

--- .github/workflows/run_tests.py
+++ .github/workflows/run_tests.py
@@ -36,7 +36,38 @@ def main():
     if os.name == 'nt':
         subprocess.call(['pgsql/bin/postgres', '-V'])
         subprocess.call(['pgsql/bin/pg_ctl', 'initdb', '-D', 'fake', '-U', 'postgres'])
-        ret = 0
+        env['COMSPEC'] = sys.executable + '"' + ' "' + os.path.abspath('.github/workflows/run_behave_windows.py')
+        print('"' + env['COMSPEC'] + '"')
+        print(subprocess.call(['pgsql/bin/pg_ctl', '-W', '-D', 'fake', '-l', 'behave.log', 'start'], env=env))
+        for _ in range(0, 60):
+            try:
+                with open('behave.log') as f:
+                    p = 0
+                    for _ in range(0, 600):
+                        f.seek(p)
+                        latest_data = f.read()
+                        p = f.tell()
+                        if latest_data:
+                            print(latest_data)
+                        elif os.path.exists('behave.exit'):
+                            break
+                        time.sleep(1)
+                    try:
+                        with open('behave.exit') as f:
+                            ret = int(f.read())
+                    except Exception as e:
+                        print(str(e))
+                        time.sleep(1)
+                    break
+            except Exception as e:
+                print(str(e))
+                time.sleep(1)
+            try:
+                with open('behave.exit') as f:
+                    ret = int(f.read())
+            except Exception as e:
+                print(str(e))
+                time.sleep(1)
     else:
         ret = subprocess.call(unbuffer + [sys.executable, '-m', 'behave'], env=env)
 
