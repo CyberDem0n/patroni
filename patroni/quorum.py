d70ae2939c05b53e7a7189fc82fbcccf694a84cd
Alexander Kukushkin <cyberdemn@gmail.com>
2023-03-15 16:27:30 +0100

    Factor out leader

--- patroni/quorum.py
+++ patroni/quorum.py
@@ -24,8 +24,8 @@ class QuorumStateResolver(object):
     is one optimal state satisfying this condition. Given the node set `active`, the optimal state is:
 
         sync = voters = active
-        numsync = min(replication_factor, len(active))
-        quorum = len(active) + 1 - numsync
+        numsync = min(sync_wanted, len(active))
+        quorum = len(active) - numsync
 
     We need to be able to produce a series of state changes that take the system to this desired state from any
     other state arbitrary given arbitrary changes is node availability, configuration and interrupted transitions.
@@ -35,50 +35,54 @@ class QuorumStateResolver(object):
 
     Order of adding or removing nodes from sync and voters depends on the state of synchronous_standby_names:
     When adding new nodes:
-        if there is only on node in sync (synchronous_standby_names is empty):
+        if sync (synchronous_standby_names) is empty:
             add new nodes first to sync and than to voters
         else:
             add new nodes first to voters and than to sync
     When removing nodes:
-        if sync will contain just a single value after removal (synchronous_standby_names becomes empty):
+        if sync (synchronous_standby_names) will become empty after removal:
             first remove nodes from voters and than from sync
         else:
             first remove nodes from sync and than from voters
     """
 
-    def __init__(self, quorum, voters, numsync, sync, active, sync_wanted):
-        self.quorum = quorum
-        self.voters = set(voters)
-        self.numsync = numsync
-        self.sync = set(sync)
-        self.active = active
-        self.sync_wanted = sync_wanted
+    def __init__(self, leader, quorum, voters, numsync, sync, active, sync_wanted, leader_wanted=None):
+        self.leader = leader
+        self.quorum = quorum                 # The number of nodes we need to check when doing leader race
+        self.voters = set(voters)            # List of nodes we need to check (both stored in the /sync key)
+        self.numsync = numsync               # The number of sync nodes in synchronous_standby_names
+        self.sync = set(sync)                # List of nodes in synchronous_standby_names
+        self.active = active                 # List of active nodes from pg_stat_replication
+        self.sync_wanted = sync_wanted       # The desired number of sync nodes
+        self.leader_wanted = leader_wanted or leader
 
     def check_invariants(self):
-        if len(self.voters) > 1 and not (len(self.voters | self.sync) < self.quorum + self.numsync):
+        if self.voters and not (len(self.voters | self.sync) <= self.quorum + self.numsync):
             raise QuorumError("Quorum and sync not guaranteed to overlap: nodes %d >= quorum %d + sync %d" %
                               (len(self.voters | self.sync), self.quorum, self.numsync))
         if not (self.voters <= self.sync or self.sync <= self.voters):
             raise QuorumError("Mismatched sets: quorum only=%s sync only=%s" %
                               (self.voters - self.sync, self.sync - self.voters))
 
-    def quorum_update(self, quorum, voters):
-        if quorum < 1:
-            raise QuorumError("Quorum %d < 1 of (%s)" % (quorum, voters))
+    def quorum_update(self, quorum, voters, leader=None):
+        if quorum < 0:
+            raise QuorumError("Quorum %d < 0 of (%s)" % (quorum, voters))
+        if leader is not None:
+            self.leader = leader
         self.quorum = quorum
         self.voters = voters
         self.check_invariants()
-        logger.debug('quorum %s %s', self.quorum, self.voters)
-        return 'quorum', self.quorum, self.voters
+        logger.debug('quorum %s %s %s', self.leader, self.quorum, self.voters)
+        return 'quorum', self.leader, self.quorum, self.voters
 
     def sync_update(self, numsync, sync):
-        if numsync < 1:
-            raise QuorumError("Sync %d < 1 of (%s)" % (numsync, sync))
+        if numsync < 0:
+            raise QuorumError("Sync %d < 0 of (%s)" % (numsync, sync))
         self.numsync = numsync
         self.sync = sync
         self.check_invariants()
-        logger.debug('sync %s %s', self.numsync, self.sync)
-        return 'sync', self.numsync, self.sync
+        logger.debug('sync %s %s %s', self.leader, self.numsync, self.sync)
+        return 'sync', self.leader, self.numsync, self.sync
 
     def __iter__(self):
         transitions = list(self._generate_transitions())
@@ -90,13 +94,17 @@ class QuorumStateResolver(object):
             yield cur_transition
 
     def _generate_transitions(self):
-        logger.debug("Quorum state: quorum %s, voters %s, numsync %s, sync %s, active %s, sync_wanted %s",
-                     self.quorum, self.voters, self.numsync, self.sync, self.active, self.sync_wanted)
+        logger.debug("Quorum state: leader %s quorum %s, voters %s, numsync %s, sync %s, active %s, sync_wanted %s leader_wanted %s",
+                     self.leader, self.quorum, self.voters, self.numsync, self.sync, self.active, self.sync_wanted, self.leader_wanted)
         try:
             self.check_invariants()
         except QuorumError as e:
             logger.warning('%s', e)
-            yield self.quorum_update(len(self.sync) + 1 - self.numsync, self.sync)
+            yield self.quorum_update(len(self.sync) - self.numsync, self.sync)
+
+        # If leader changes we need to add the old leader to quorum (voters)
+        if self.leader_wanted != self.leader:
+            yield self.quorum_update(self.quorum, self.voters | set([self.leader]), self.leader_wanted)
 
         # Handle non steady state cases
         if self.sync < self.voters:
@@ -106,7 +114,7 @@ class QuorumStateResolver(object):
             remove_from_quorum = self.voters - (self.sync | self.active)
             if remove_from_quorum:
                 yield self.quorum_update(
-                    quorum=len(self.voters) - len(remove_from_quorum) + 1 - self.numsync,
+                    quorum=len(self.voters) - len(remove_from_quorum) - self.numsync,
                     voters=self.voters - remove_from_quorum)
             # Start syncing to nodes that are in quorum and alive
             add_to_sync = self.voters - self.sync
@@ -132,12 +140,12 @@ class QuorumStateResolver(object):
         assert self.voters == self.sync
 
         safety_margin = self.quorum + self.numsync - len(self.voters | self.sync)
-        if safety_margin > 1:  # In the middle of changing replication factor.
+        if safety_margin > 0:  # In the middle of changing replication factor.
             logger.debug("Case 3: quorum ot replication factor is bigger than needed")
 
             # If we were increasing replication factor the below line will finish transition
             # In case if it was decrease operation we rollback quorum and let remaining code resolve transition
-            yield self.quorum_update(len(self.voters) + 1 - self.numsync, self.voters)
+            yield self.quorum_update(len(self.voters) - self.numsync, self.voters)
 
         # We are in a steady state point. Find if desired state is different and act accordingly.
 
@@ -145,20 +153,20 @@ class QuorumStateResolver(object):
         to_remove = self.sync - self.active
         if to_remove:
             logger.debug("Removing nodes: %s", to_remove)
-            can_reduce_quorum_by = self.quorum - 1
+            can_reduce_quorum_by = self.quorum
             # If we can reduce quorum size try to do so first
             if can_reduce_quorum_by:
                 # Pick nodes to remove by sorted order to provide deterministic behavior for tests
                 remove = set(sorted(to_remove, reverse=True)[:can_reduce_quorum_by])
                 sync = self.sync - remove
-                # we can safely increase numsync if requested
-                numsync = min(self.sync_wanted, len(sync) + 1) if self.sync_wanted > self.numsync else self.numsync
+                # when removing nodes from sync we can safely increase numsync if requested
+                numsync = min(self.sync_wanted, len(sync)) if self.sync_wanted > self.numsync else self.numsync
                 yield self.sync_update(numsync, sync)
                 voters = self.voters - remove
-                yield self.quorum_update(len(voters) + 1 - self.numsync, voters)
+                yield self.quorum_update(len(voters) - self.numsync, voters)
                 to_remove &= self.sync
             if to_remove:
-                assert self.quorum == 1
+                assert self.quorum == 0
                 yield self.quorum_update(self.quorum, self.voters - to_remove)
                 yield self.sync_update(self.numsync - len(to_remove), self.sync - to_remove)
 
@@ -170,18 +178,18 @@ class QuorumStateResolver(object):
             sync_wanted = min(self.sync_wanted, len(self.sync | to_add))
             increase_numsync_by = sync_wanted - self.numsync
             if increase_numsync_by > 0:
-                if len(self.sync) == 1:  # there is only the leader
-                    add = to_add         # and it is safe to add all nodes at once
-                else:
+                if self.sync:
                     add = set(sorted(to_add)[:increase_numsync_by])
                     increase_numsync_by = len(add)
+                else:  # there is only the leader
+                    add = to_add  # and it is safe to add all nodes at once if sync is empty
                 yield self.sync_update(self.numsync + increase_numsync_by, self.sync | add)
                 voters = self.voters | add
-                yield self.quorum_update(len(voters) + 1 - sync_wanted, voters)
+                yield self.quorum_update(len(voters) - sync_wanted, voters)
                 to_add -= self.sync
             if to_add:
                 voters = self.voters | to_add
-                yield self.quorum_update(len(voters) + 1 - sync_wanted, voters)
+                yield self.quorum_update(len(voters) - sync_wanted, voters)
                 yield self.sync_update(sync_wanted, self.sync | to_add)
 
         # Apply requested replication factor change
@@ -190,9 +198,9 @@ class QuorumStateResolver(object):
             # Increase replication factor
             logger.debug("Increasing replication factor to %s", self.numsync + sync_increase)
             yield self.sync_update(self.numsync + sync_increase, self.sync)
-            yield self.quorum_update(len(self.voters) + 1 - self.numsync, self.voters)
+            yield self.quorum_update(len(self.voters) - self.numsync, self.voters)
         elif sync_increase < 0:
             # Reduce replication factor
             logger.debug("Reducing replication factor to %s", self.numsync + sync_increase)
-            yield self.quorum_update(len(self.voters) + 1 - self.numsync - sync_increase, self.voters)
+            yield self.quorum_update(len(self.voters) - self.numsync - sync_increase, self.voters)
             yield self.sync_update(self.numsync + sync_increase, self.sync)
--- tests/test_quorum.py
+++ tests/test_quorum.py
@@ -5,175 +5,181 @@ from patroni.quorum import QuorumStateResolver, QuorumError
 
 class QuorumTest(unittest.TestCase):
     def test_1111(self):
-        state = 1, set("a"), 1, set("a")
-        self.assertEqual(list(QuorumStateResolver(*state, active=set("ab"), sync_wanted=3)), [
-            ('sync', 2, set('ab')),
-            ('quorum', 1, set('ab')),
+        leader = 'a'
+        state = leader, 0, set(""), 0, set("")
+        self.assertEqual(list(QuorumStateResolver(*state, active=set("b"), sync_wanted=2, leader_wanted=leader)), [
+            ('sync', leader, 1, set('b')),
+            ('quorum', leader, 0, set('b')),
         ])
-        self.assertEqual(list(QuorumStateResolver(*state, active=set("abcde"), sync_wanted=3)), [
-            ('sync', 3, set('abcde')),
-            ('quorum', 3, set('abcde')),
+        self.assertEqual(list(QuorumStateResolver(*state, active=set("bcde"), sync_wanted=2, leader_wanted=leader)), [
+            ('sync', leader, 2, set('bcde')),
+            ('quorum', leader, 2, set('bcde')),
         ])
 
     def test_1222(self):
         """2 node cluster"""
-        state = 1, set("ab"), 2, set("ab")
+        leader = 'a'
+        state = leader, 0, set("b"), 1, set("b")
         # Active set matches state
-        self.assertEqual(list(QuorumStateResolver(*state, active=set("ab"), sync_wanted=3)), [
+        self.assertEqual(list(QuorumStateResolver(*state, active=set("b"), sync_wanted=2, leader_wanted=leader)), [
         ])
         # Add node by increasing quorum
-        self.assertEqual(list(QuorumStateResolver(*state, active=set("abc"), sync_wanted=2)), [
-            ('quorum', 2, set('abc')),
-            ('sync', 2, set('abc')),
+        self.assertEqual(list(QuorumStateResolver(*state, active=set("bc"), sync_wanted=1, leader_wanted=leader)), [
+            ('quorum', leader, 1, set('bc')),
+            ('sync', leader, 1, set('bc')),
         ])
         # Add node by increasing sync
-        self.assertEqual(list(QuorumStateResolver(*state, active=set("abc"), sync_wanted=3)), [
-            ('sync', 3, set('abc')),
-            ('quorum', 1, set('abc')),
+        self.assertEqual(list(QuorumStateResolver(*state, active=set("bc"), sync_wanted=2, leader_wanted=leader)), [
+            ('sync', leader, 2, set('bc')),
+            ('quorum', leader, 0, set('bc')),
         ])
         # Add multiple nodes by increasing both sync and quorum
-        self.assertEqual(list(QuorumStateResolver(*state, active=set("abcde"), sync_wanted=3)), [
-            ('sync', 3, set('abc')),
-            ('quorum', 3, set('abcde')),
-            ('sync', 3, set('abcde')),
+        self.assertEqual(list(QuorumStateResolver(*state, active=set("bcde"), sync_wanted=2, leader_wanted=leader)), [
+            ('sync', leader, 2, set('bc')),
+            ('quorum', leader, 2, set('bcde')),
+            ('sync', leader, 2, set('bcde')),
         ])
         # Master is alone
-        self.assertEqual(list(QuorumStateResolver(*state, active=set("a"), sync_wanted=2)), [
-            ('quorum', 1, set('a')),
-            ('sync', 1, set('a')),
+        self.assertEqual(list(QuorumStateResolver(*state, active=set(""), sync_wanted=1, leader_wanted=leader)), [
+            ('quorum', leader, 0, set('')),
+            ('sync', leader, 0, set('')),
         ])
         # Swap out sync replica
-        self.assertEqual(list(QuorumStateResolver(*state, active=set("ac"), sync_wanted=2)), [
-            ('quorum', 1, set('a')),
-            ('sync', 2, set('ac')),
-            ('quorum', 1, set('ac')),
+        self.assertEqual(list(QuorumStateResolver(*state, active=set("c"), sync_wanted=1, leader_wanted=leader)), [
+            ('quorum', leader, 0, set('')),
+            ('sync', leader, 1, set('c')),
+            ('quorum', leader, 0, set('c')),
         ])
 
     def test_1233(self):
         """Interrupted transition from 2 node cluster to 3 node fully sync cluster"""
-        state = 1, set("ab"), 3, set("abc")
+        leader = 'a'
+        state = leader, 0, set("b"), 2, set("bc")
         # Node c went away, transition back to 2 node cluster
-        self.assertEqual(list(QuorumStateResolver(*state, active=set("ab"), sync_wanted=3)), [
-            ('sync', 2, set('ab')),
+        self.assertEqual(list(QuorumStateResolver(*state, active=set("b"), sync_wanted=2, leader_wanted=leader)), [
+            ('sync', leader, 1, set('b')),
         ])
         # Node c is available transition to larger quorum set.
-        self.assertEqual(list(QuorumStateResolver(*state, active=set("abc"), sync_wanted=3)), [
-            ('quorum', 1, set('abc')),
+        self.assertEqual(list(QuorumStateResolver(*state, active=set("bc"), sync_wanted=2, leader_wanted=leader)), [
+            ('quorum', leader, 0, set('bc')),
         ])
         # Add in a new node at the same time
-        self.assertEqual(list(QuorumStateResolver(*state, active=set("abcd"), sync_wanted=3)), [
-            ('quorum', 2, set('abcd')),
-            ('sync', 3, set('abcd')),
+        self.assertEqual(list(QuorumStateResolver(*state, active=set("bcd"), sync_wanted=2, leader_wanted=leader)), [
+            ('quorum', leader, 1, set('bcd')),
+            ('sync', leader, 2, set('bcd')),
         ])
         # Change replication factor at the same time
-        self.assertEqual(list(QuorumStateResolver(*state, active=set("abc"), sync_wanted=2)), [
-            ('quorum', 2, set('abc')),
-            ('sync', 2, set('abc')),
+        self.assertEqual(list(QuorumStateResolver(*state, active=set("bc"), sync_wanted=1, leader_wanted=leader)), [
+            ('quorum', leader, 1, set('bc')),
+            ('sync', leader, 1, set('bc')),
         ])
 
     def test_2322(self):
         """Interrupted transition from 2 node cluster to 3 node cluster with replication factor 2"""
-        state = 2, set("abc"), 2, set("ab")
+        leader = 'a'
+        state = leader, 1, set("bc"), 1, set("b")
         # Node c went away, transition back to 2 node cluster
-        self.assertEqual(list(QuorumStateResolver(*state, active=set("ab"), sync_wanted=2)), [
-            ('quorum', 1, set('ab')),
+        self.assertEqual(list(QuorumStateResolver(*state, active=set("b"), sync_wanted=1, leader_wanted=leader)), [
+            ('quorum', leader, 0, set('b')),
         ])
         # Node c is available transition to larger quorum set.
-        self.assertEqual(list(QuorumStateResolver(*state, active=set("abc"), sync_wanted=2)), [
-            ('sync', 2, set('abc')),
+        self.assertEqual(list(QuorumStateResolver(*state, active=set("bc"), sync_wanted=1, leader_wanted=leader)), [
+            ('sync', leader, 1, set('bc')),
         ])
         # Add in a new node at the same time
-        self.assertEqual(list(QuorumStateResolver(*state, active=set("abcd"), sync_wanted=2)), [
-            ('sync', 2, set('abc')),
-            ('quorum', 3, set('abcd')),
-            ('sync', 2, set('abcd')),
+        self.assertEqual(list(QuorumStateResolver(*state, active=set("bcd"), sync_wanted=1, leader_wanted=leader)), [
+            ('sync', leader, 1, set('bc')),
+            ('quorum', leader, 2, set('bcd')),
+            ('sync', leader, 1, set('bcd')),
         ])
         # Convert to a fully synced cluster
-        self.assertEqual(list(QuorumStateResolver(*state, active=set("abc"), sync_wanted=3)), [
-            ('sync', 3, set('abc')),
-            ('quorum', 1, set('abc')),
+        self.assertEqual(list(QuorumStateResolver(*state, active=set("bc"), sync_wanted=2, leader_wanted=leader)), [
+            ('sync', leader, 2, set('bc')),
+            ('quorum', leader, 0, set('bc')),
         ])
 
     def test_3535(self):
-        state = 3, set("abcde"), 3, set("abcde")
+        leader = 'a'
+        state = leader, 2, set("bcde"), 2, set("bcde")
         # remove nodes
-        self.assertEqual(list(QuorumStateResolver(*state, active=set("abc"), sync_wanted=3)), [
-            ('sync', 3, set('abc')),
-            ('quorum', 1, set('abc')),
+        self.assertEqual(list(QuorumStateResolver(*state, active=set("bc"), sync_wanted=2, leader_wanted=leader)), [
+            ('sync', leader, 2, set('bc')),
+            ('quorum', leader, 0, set('bc')),
         ])
-        self.assertEqual(list(QuorumStateResolver(*state, active=set("abcd"), sync_wanted=3)), [
-            ('sync', 3, set('abcd')),
-            ('quorum', 2, set('abcd')),
+        self.assertEqual(list(QuorumStateResolver(*state, active=set("bcd"), sync_wanted=2, leader_wanted=leader)), [
+            ('sync', leader, 2, set('bcd')),
+            ('quorum', leader, 1, set('bcd')),
         ])
 
         # remove nodes and decrease sync
-        self.assertEqual(list(QuorumStateResolver(*state, active=set("abc"), sync_wanted=2)), [
-            ('sync', 3, set('abc')),
-            ('quorum', 2, set('abc')),
-            ('sync', 2, set('abc')),
+        self.assertEqual(list(QuorumStateResolver(*state, active=set("bc"), sync_wanted=1, leader_wanted=leader)), [
+            ('sync', leader, 2, set('bc')),
+            ('quorum', leader, 1, set('bc')),
+            ('sync', leader, 1, set('bc')),
         ])
 
         # Increase replication factor and decrease quorum
-        self.assertEqual(list(QuorumStateResolver(*state, active=set('abcde'), sync_wanted=4)), [
-            ('sync', 4, set('abcde')),
-            ('quorum', 2, set('abcde')),
+        self.assertEqual(list(QuorumStateResolver(*state, active=set('bcde'), sync_wanted=3, leader_wanted=leader)), [
+            ('sync', leader, 3, set('bcde')),
+            ('quorum', leader, 1, set('bcde')),
         ])
 
         # Add node with decreasing sync and increasing quorum
-        self.assertEqual(list(QuorumStateResolver(*state, active=set('abcdef'), sync_wanted=2)), [
-            ('quorum', 5, set('abcdef')),   # increase quorum one more time
-            ('sync', 2, set('abcdef')),     # now reduce replication factor to requested value
+        self.assertEqual(list(QuorumStateResolver(*state, active=set('bcdef'), sync_wanted=1, leader_wanted=leader)), [
+            ('quorum', leader, 4, set('bcdef')),   # increase quorum one more time
+            ('sync', leader, 1, set('bcdef')),     # now reduce replication factor to requested value
         ])
 
         # Remove node with increasing sync and decreasing quorum
-        self.assertEqual(list(QuorumStateResolver(*state, active=set('abcd'), sync_wanted=4)), [
-            ('sync', 4, set('abcd')),       # node e removed from sync wth replication factor increase
-            ('quorum', 1, set('abcd')),     # node e removed from voters with quorum decrease
+        self.assertEqual(list(QuorumStateResolver(*state, active=set('bcd'), sync_wanted=3, leader_wanted=leader)), [
+            ('sync', leader, 3, set('bcd')),       # node e removed from sync wth replication factor increase
+            ('quorum', leader, 0, set('bcd')),     # node e removed from voters with quorum decrease
         ])
 
     def test_nonsync_promotion(self):
-        # Beginning state: 2 of abc in sync. e.g. (a master, ssn = ANY 1 (b c))
+        # Beginning state: 1 of bc in sync. e.g. (a primary, ssn = ANY 1 (b c))
         # a fails, d sees b and c, knows that it is in sync and decides to promote.
-        # We include in sync state everybody that already was in sync, increasing replication factor
-        # and let situation resolve. Node d ssn=ANY 2 (a b c)
-        state = 2, set("abc"), 3, set("abcd")
-        self.assertEqual(list(QuorumStateResolver(*state, active=set("bcd"), sync_wanted=2)), [
-            ('quorum', 2, set('abcd')),  # Set ourselves to be a member of the quorum
-            ('sync', 3, set('bcd')),     # Remove a from being synced to.
-            ('quorum', 2, set('bcd')),   # Remove a from quorum
-            ('sync', 2, set('bcd')),     # Can now reduce replication factor to original value
+        # We include in sync state former primary increasing replication factor
+        # and let situation resolve. Node d ssn=ANY 1 (b c)
+        leader = 'd'
+        state = 'a', 1, set("bc"), 2, set("abc")
+        self.assertEqual(list(QuorumStateResolver(*state, active=set("bc"), sync_wanted=1, leader_wanted=leader)), [
+            ('quorum', leader, 1, set('abc')),  # Set ourselves to be a member of the quorum
+            ('sync', leader, 2, set('bc')),     # Remove a from being synced to.
+            ('quorum', leader, 1, set('bc')),   # Remove a from quorum
+            ('sync', leader, 1, set('bc')),     # Can now reduce replication factor to original value
         ])
 
     def test_invalid_states(self):
+        leader = 'a'
         # Main invariant is not satisfied, system is in an unsafe state
-        resolver = QuorumStateResolver(1, set("abc"), 2, set("abc"), active=set("abc"), sync_wanted=2)
+        resolver = QuorumStateResolver(leader, 0, set("bc"), 1, set("bc"),
+                                       active=set("bc"), sync_wanted=1, leader_wanted=leader)
         self.assertRaises(QuorumError, resolver.check_invariants)
         self.assertEqual(list(resolver), [
-            ('quorum', 2, set('abc'))
+            ('quorum', leader, 1, set('bc'))
         ])
         # Quorum and sync states mismatched, somebody other than Patroni modified system state
-        resolver = QuorumStateResolver(2, set("abc"), 3, set("abd"), active=set("abd"), sync_wanted=2)
+        resolver = QuorumStateResolver(leader, 1, set("bc"), 2, set("bd"),
+                                       active=set("bd"), sync_wanted=1, leader_wanted=leader)
         self.assertRaises(QuorumError, resolver.check_invariants)
         self.assertEqual(list(resolver), [
-            ('quorum', 2, set('abd')),
-            ('sync', 2, set('abd')),
+            ('quorum', leader, 1, set('bd')),
+            ('sync', leader, 1, set('bd')),
         ])
 
-    def test_sync_high_quorum_low_safety_margin_not_1(self):
-        state = 3, set('abcdef'), 5, set('abcdef')
-        self.assertEqual(list(QuorumStateResolver(*state, active=set("abcdef"), sync_wanted=3)), [
-            ('quorum', 4, set('abcdef')),  # Adjust quorum requirements
-            ('sync', 3, set('abcdef')),    # Reduce synchronization
-        ])
-        self.assertEqual(list(QuorumStateResolver(*state, active=set("abcdef"), sync_wanted=1)), [
-            ('quorum', 6, set('abcdef')),  # Adjust quorum requirements
-            ('sync', 1, set('abcdef')),    # Reduce synchronization
+    def test_sync_high_quorum_low_safety_margin_high(self):
+        leader = 'a'
+        state = leader, 2, set('bcdef'), 4, set('bcdef')
+        self.assertEqual(list(QuorumStateResolver(*state, active=set("bcdef"), sync_wanted=2, leader_wanted=leader)), [
+            ('quorum', leader, 3, set('bcdef')),  # Adjust quorum requirements
+            ('sync', leader, 2, set('bcdef')),    # Reduce synchronization
         ])
 
     def test_quorum_update(self):
-        resolver = QuorumStateResolver(2, set('abc'), 2, set('abc'), set('abc'), 2)
-        self.assertRaises(QuorumError, resolver.quorum_update, 0, set())
+        resolver = QuorumStateResolver('a', 1, set('bc'), 1, set('bc'), set('bc'), 1)
+        self.assertRaises(QuorumError, resolver.quorum_update, -1, set())
 
     def test_sync_update(self):
-        resolver = QuorumStateResolver(2, set('abc'), 2, set('abc'), set('abc'), 2)
-        self.assertRaises(QuorumError, resolver.sync_update, 0, set())
+        resolver = QuorumStateResolver('a', 1, set('bc'), 1, set('bc'), set('bc'), 1)
+        self.assertRaises(QuorumError, resolver.sync_update, -1, set())
